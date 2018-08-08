0b553c62628a5c5e0b64135f4b52ac50b0dabad2
ehfhcrc <ehenrich@fredhutch.org>
2018-08-08 12:34:33 -0700

    Make copyMatricesToVirtualSdy a fn

--- R/CopyMatricesToVirtualSdy.R
+++ R/CopyMatricesToVirtualSdy.R
@@ -1,43 +1,47 @@
-library(Rlabkey)
+copyMatricesToVirtualSdy <- function(baseUrl, virtualSdy){
+  library(Rlabkey)
+  runs <- labkey.selectRows(baseUrl = baseUrl,
+                            folderPath = "/Studies/",
+                            schemaName = "assay.ExpressionMatrix.matrix",
+                            queryName = "Runs",
+                            showHidden = TRUE)
 
-baseUrl <- "https://test.immunespace.org"
-# get runs from /Studies
-runs <- labkey.selectRows(baseUrl = baseUrl,
-                          folderPath = "/Studies/",
-                          schemaName = "assay.ExpressionMatrix.matrix",
-                          queryName = "Runs",
-                          showHidden = TRUE)
+  # subset to those in vSdy
+  vSdyList <- list()
+  vSdyList[["IS2"]] <- c("SDY56, SDY61, SDY63, SDY67, SDY80, SDY180, SDY212, SDY224, SDY269, SDY270, SDY400, SDY404, SDY520, SDY984, SDY1260, SDY1264, SDY1276, SDY1289, SDY1293")
+  vSdyList[["IS1"]]  <- c("SDY63","SDY67","SDY80","SDY212","SDY400","SDY404")
+  studies <- vSdyList[[virtualSdy]]
+  studies <- strsplit(studies, ", ")[[1]]
+  runs <- runs[ runs$Study %in% studies, ]
 
-# subset to those in IS2
-studies <- c("SDY56, SDY61, SDY63, SDY67, SDY80, SDY180, SDY212, SDY224, SDY269, SDY270, SDY400, SDY404, SDY520, SDY984, SDY1260, SDY1264, SDY1276, SDY1289, SDY1293")
-studies <- strsplit(studies, ", ")[[1]]
-runs <- runs[ runs$Study %in% studies, ]
+  # for each run cp over the .tsv.raw, .tsv.summary, .tsv.summary.orig files
+  root <- "/share/files/Studies/"
+  mid <- "/@files/analysis/exprs_matrices/"
 
-# for each run cp over the .tsv.raw, .tsv.summary, .tsv.summary.orig files
-root <- "/share/files/Studies/"
-mid <- "/@files/analysis/exprs_matrices/"
+  vPath <- paste0("/share/files/HIPC/", virtualSdy, "/@files/analysis/exprs_matrices/")
+  for(i in 1:nrow(runs)){
+    x <- runs[i, ]
+    print(x)
 
-vPath <- "/share/files/HIPC/IS2/@files/analysis/exprs_matrices/"
-for(i in 1:nrow(runs)){
-  x <- runs[i, ]
+    mxPathOnSdy <- paste0(root, x$Study, mid, x$Name, ".tsv")
+    mxPathOnVirt <- paste0(vPath, "Run", x$`Row Id`, "/", x$Name, ".tsv")
 
-  mxPathOnSdy <- paste0(root, x$Study, mid, x$Name, ".tsv")
-  mxPathOnVirt <- paste0(vPath, "Run", x$`Row Id`, "/", x$Name, ".tsv")
+    # the latest / normalized .tsv are not same?
+    file.copy(from = mxPathOnSdy,
+              to = mxPathOnVirt,
+              overwrite = TRUE)
 
-  # the latest / normalized .tsv are not same?
-  file.copy(from = mxPathOnSdy,
-            to = mxPathOnVirt,
-            overwrite = TRUE)
+    file.copy(from = paste0(mxPathOnSdy, ".raw"),
+              to = paste0(mxPathOnVirt, ".raw"),
+              overwrite = FALSE)
 
-  file.copy(from = paste0(mxPathOnSdy, ".raw"),
-            to = paste0(mxPathOnVirt, ".raw"),
-            overwrite = FALSE)
+    file.copy(from = paste0(mxPathOnSdy, ".summary"),
+              to = paste0(mxPathOnVirt, ".summary"),
+              overwrite = FALSE)
 
-  file.copy(from = paste0(mxPathOnSdy, ".summary"),
-            to = paste0(mxPathOnVirt, ".summary"),
-            overwrite = FALSE)
-
-  file.copy(from = paste0(mxPathOnSdy, ".summary.orig"),
-            to = paste0(mxPathOnVirt, ".summary.orig"),
-            overwrite = FALSE)
+    file.copy(from = paste0(mxPathOnSdy, ".summary.orig"),
+              to = paste0(mxPathOnVirt, ".summary.orig"),
+              overwrite = FALSE)
+  }
 }
+
