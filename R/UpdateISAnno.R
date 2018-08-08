7d2b4673177e14fd9a23ac7eebaf754f7c778bcf
ehfhcrc <ehenrich@fredhutch.org>
2018-08-08 13:39:13 -0700

    ensure updateGEAR con baseUrl

--- R/UpdateISAnno.R
+++ R/UpdateISAnno.R
@@ -286,7 +286,8 @@ updateGEAR <- function(sdy, baseUrl, runsDF){
   contrast <- c("study_time_collected", "study_time_collected_unit")
   labkey.url.base <- baseUrl
   labkey.url.path <- paste0("/Studies/", sdy)
-  con <- CreateConnection()
+  # can't use CreateConnection() b/c lup/lub not in global env
+  con <- CreateConnection(study = sdy, onTest = grepl("test", baseUrl))
   con$getGEInputs()
   GEA_list <- vector("list")
   GEAR_list <- vector("list")
