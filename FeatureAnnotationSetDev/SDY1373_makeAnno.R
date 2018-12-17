391e1cb27c4138f959fe57abe36def152a0f3156
ehfhcrc <ehenrich@fredhutch.org>
2018-12-17 15:09:23 -0800

    add HTv4 manifest

--- FeatureAnnotationSetDev/SDY1373_makeAnno.R
+++ FeatureAnnotationSetDev/SDY1373_makeAnno.R
@@ -1,6 +1,6 @@
 # Evan Henrich
 # Oct 2018
-# Notes: SDY1264 used ensembl version 79 according to the GSE accession: 
+# Notes: SDY1373 used ensembl version 79 according to the GSE accession:
 # https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE97590
 
 library(biomaRt)
@@ -8,14 +8,15 @@ library(data.table)
 
 # Get ensembl id and gene id
 mart <- useEnsembl(biomart = "ensembl", version = 79, dataset = "hsapiens_gene_ensembl")
-ens2gene <- getBM(attributes=c('ensembl_gene_id','hgnc_symbol'), 
+ens2gene <- getBM(attributes=c('ensembl_gene_id','hgnc_symbol'),
              mart = mart)
 
-# Remove unmapped
+# Remove unmapped and duplicated probe ids
 res <- data.frame(Probe_ID = ens2gene$ensembl_gene_id,
                   Gene_Symbol = ens2gene$hgnc_symbol,
                   stringsAsFactors = F)
 res <- res[ !is.na(res$Gene_Symbol), ]
+res <- res[ !duplicated(res$Probe_ID), ]
 
 # Write out
 write.table(res,
