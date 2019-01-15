d7c21b921e66d5a97395bb37e2ef09946868b58e
ehfhcrc <ehenrich@fredhutch.org>
2019-01-15 13:38:04 -0800

    Add anno dev scripts for HEEBOHumanSetV1 and SDY1368_customAnno

--- FeatureAnnotationSetDev/SDY1368_makeAnno.R
+++ FeatureAnnotationSetDev/SDY1368_makeAnno.R
@@ -5,10 +5,13 @@
 
 library(data.table)
 
-# get unigene from norm_exprs by cutting off suffix of probes
 # SDY1368_EM.txt was just renamed from original supp file from GSE
 # GSE86331_P98_2011DS75_Stanford_Batch16_2HT12_V4_BM_V4_0_R1_20Sept2011_Raw_Data_GX11.txt.gz
-em <- data.table::fread("FeatureAnnotationSetDev/SDY1368_EM.txt")
+link <- "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE86331&format=file&file=GSE86331%5FP98%5F2011DS75%5FStanford%5FBatch16%5F2HT12%5FV4%5FBM%5FV4%5F0%5FR1%5F20Sept2011%5FRaw%5FData%5FGX11%2Etxt%2Egz"
+fl <- "FeatureAnnotationSetDev/SDY1368_EM.txt.gz"
+download.file(link, fl)
+GEOquery::gunzip(fl)
+em <- fread(gsub("\\.gz", "", fl))
 
 # Remove unmapped
 res <- data.frame(Probe_ID = em$ProbeID,
@@ -22,3 +25,6 @@ write.table(res,
             quote = FALSE,
             sep = "\t",
             row.names = FALSE)
+
+# clean up
+file.remove(gsub("\\.gz", "", fl))
